dafbf309e9166348a3afc49ceb6df6db15f2bcb7
Rebecca Stevens <rebeccastevens@catalyst.net.nz>
2018-02-14 14:42:06 +1300

    Polymer elements are now loaded when ready as they auto register.

--- demo/import.js
+++ demo/import.js
@@ -1,14 +1,20 @@
 // Import the helpers.
 import '../node_modules/@polymer/iron-demo-helpers/demo-snippet.js';
 
-// Import the element.
-import { CatalystToggleButton } from '../node_modules/@catalyst-elements/catalyst-toggle-button/dist/catalyst-toggle-button.module.js';
+/**
+ * Load the polymer elements.
+ */
+function loadPolymerElements() {
+  import('../node_modules/@polymer/iron-demo-helpers/demo-snippet.js');
+}
 
 // Register the element.
 if (window.WebComponents === undefined || window.WebComponents.ready) {
   CatalystToggleButton.register();
+  loadPolymerElements();
 } else {
   window.addEventListener('WebComponentsReady', () => {
     CatalystToggleButton.register();
+    loadPolymerElements();
   });
 }
